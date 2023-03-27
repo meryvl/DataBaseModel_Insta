074e10523c57b52ebfd8ed417a0c1f0a73a22882
meryvl <meryvalerolaguna@gmail.com>
2023-03-27 22:45:09 +0000

    commit

--- src/models.py
+++ src/models.py
@@ -8,26 +8,46 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'User'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(250), nullable=False)
+    firstname = Column(String(250), nullable=False)
+    lastname = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Follower(Base):
+    __tablename__ = 'Follower'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    User_id = Column(Integer, ForeignKey('User.id'))
+    User = relationship(User)
 
+class Post(Base):
+    __tablename__ = 'Post'
+    id = Column(Integer, primary_key=True)
+    User_id = Column(Integer, ForeignKey('User.id'))
+    User = relationship(User)
+
+class Media(Base):
+    __tablename__ = 'Media'
+    id = Column(Integer, primary_key=True)
+    type= Column(String(250), nullable=False)
+    url= Column(String(250), nullable=False)
+    Post_id = Column(Integer, ForeignKey('Post.id'))
+    Post = relationship(Post)
+
+class Comment(Base):
+    __tablename__ = 'Comment'
+    id = Column(Integer, primary_key=True)
+    Post_id = Column(Integer, ForeignKey('Post.id'))
+    Post = relationship(Post)
+    author_id = Column(Integer, ForeignKey('User.id'))
+    User = relationship(User)
+
+   
     def to_dict(self):
         return {}
+
+
 
 ## Draw from SQLAlchemy base
 try:
